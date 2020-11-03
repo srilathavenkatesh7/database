0b80980b666af8024ebc335a24eafe2eff4f52bb
srilathavenkatesh7 <srilathavenkatesh7@gmail.com>
2020-11-03 21:03:26 +0530

    newproject

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
 var ball;
-
+var database,position;
 function setup(){
+    database=firebase.database();
+
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var ballposition=database.ref('ball/position').on("value",readposition,showerror);
+
 }
 
 function draw(){
@@ -22,8 +26,14 @@ function draw(){
     }
     drawSprites();
 }
+function readposition(data){
+ position=data.val();
+ ball.x=position.x;
+ ball.y=position.y;
+}
+function showerror(){}
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    database.ref('ball/position').set({'x':position.x+x,'y':position.y+y})
+   
 }
